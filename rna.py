6a129a3bd98e6272a488ab65e5a6fe361cade7c0
NetoPedro <pedroneto_09@hotmail.com>
2019-04-24 23:08:47 +0300

    [RNA] Initial basic structure of the RNA class. Added a Sequence, a Representation of the sequence and a free energy value. Also there is a skeleton for the pairing function.

--- rna.py
+++ rna.py
@@ -1,2 +1,12 @@
+import numpy as np
+SEQUENCE_LENGTH = 10
+
+
 class RNA:
-    pass
+    def __init__(self):
+        self.sequence = ""  # TODO Define the initial sequence
+        self.representation =  np.zeros() # TODO Find a proper representation (dot matrix?)
+        self.free_energy = SEQUENCE_LENGTH   # For now this represents the number of elements in the sequence - connections
+    def pairing(self, nucleo1, nucleo2):
+        pass
+
