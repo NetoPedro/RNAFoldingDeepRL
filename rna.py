836257bbfef562e2456076a981f3c6393f3e6840
NetoPedro <pedroneto_09@hotmail.com>
2019-04-24 23:18:38 +0300

    [RNA] Added some decoupling to ensure a sustained development of the projects with minimal changes of the code in case of requirements change.

--- rna.py
+++ rna.py
@@ -3,10 +3,22 @@ SEQUENCE_LENGTH = 10
 
 
 class RNA:
-    def __init__(self):
-        self.sequence = ""  # TODO Define the initial sequence
-        self.representation =  np.zeros() # TODO Find a proper representation (dot matrix?)
-        self.free_energy = SEQUENCE_LENGTH   # For now this represents the number of elements in the sequence - connections
+    def __init__(self, sequence):
+        self.sequence = sequence # TODO Define the initial sequence
+        self.representation =  self.build_representations(self.sequence)
+        self.free_energy = self.update_free_energy()
+
+
     def pairing(self, nucleo1, nucleo2):
         pass
+
+
+    def build_representations(self,sequence):
+        # TODO Find a proper representation (dot matrix?)
+        return sequence
+
+    #  This function is here for decoupling in case a new and more accurate energy function is given.
+    def update_free_energy(self,representation):
+        # For now this represents the number of elements in the sequence - connections
+        return 0
 
