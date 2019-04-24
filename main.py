2ebde436415fa26f1eeba0926ea0b0462119e46e
NetoPedro <pedroneto_09@hotmail.com>
2019-04-24 22:51:22 +0300

    [Project Structure]  Main and Train files added

---
+++ main.py
@@ -0,0 +1,9 @@
+import train
+
+def main():
+    train()
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ train.py
@@ -0,0 +1,6 @@
+# TODO Insert Constants to hold values like max iterations, number of epochs and so on
+
+
+def train():
+    pass
+
