d80d2f411b4028dc4b086c9414661b3456fa8e0d
NetoPedro <pedroneto_09@hotmail.com>
2019-04-25 11:13:02 +0300

    [RNA] Added some rules to the pairing function. It should not allow crossing pairs and it should disconnect the pairs if they were previously connected.  Also defined the free energy update function.

--- rna.py
+++ rna.py
@@ -4,21 +4,35 @@ SEQUENCE_LENGTH = 10
 
 class RNA:
     def __init__(self, sequence):
+        # if len(sequence) < SEQUENCE_LENGTH:
+        #    padding_size = SEQUENCE_LENGTH - len(sequence)
+        #    for i in range (padding_size):
+        #        sequence += 'E'
+
         self.sequence = sequence # TODO Define the initial sequence
-        self.representation =  self.build_representations(self.sequence)
+        self.structure_representation =  self.build_representations(self.sequence)
         self.free_energy = self.update_free_energy()
 
 
     def pairing(self, nucleo1, nucleo2):
-        pass
-
+        # TODO Verify bases before connecting them
+        if len(self.structure_representation) > 0 :
+            for (n1,n2) in self.structure_representation:
+                if nucleo1 < n1 and nucleo2 > n1 and nucleo2 < n2:
+                    return
+                if nucleo1 < n2 and nucleo1 > n1 and nucleo2 > n2 :
+                    return
+        if (nucleo1,nucleo2) in self.structure_representation:
+            self.structure_representation.remove((nucleo1, nucleo2))
+        else:
+            self.structure_representation.append((nucleo1, nucleo2))
+        self.update_free_energy()
 
     def build_representations(self,sequence):
-        # TODO Find a proper representation (dot matrix?)
-        return sequence
+        return []
 
     #  This function is here for decoupling in case a new and more accurate energy function is given.
-    def update_free_energy(self,representation):
+    def update_free_energy(self):
         # For now this represents the number of elements in the sequence - connections
-        return 0
+        return SEQUENCE_LENGTH - len(self.structure_representation)
 
