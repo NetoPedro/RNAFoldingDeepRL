7774959b69dfa1aa304ddfe4a13ab0c8efc7df61
NetoPedro <pedroneto_09@hotmail.com>
2019-04-25 13:05:44 +0300

    [Train] Added random sequence generator

--- train.py
+++ train.py
@@ -1,12 +1,13 @@
 import policy as policy_module
 import env_rna
-import torch.optim.Adam as adam
+import torch
+import numpy as np
 # TODO Insert Constants to hold values like max iterations, number of epochs and so on
 
 env = env_rna.EnvRNA()
 def train():
     policy = policy_module.Policy()
-    optimizer = adam(policy.parameters(), lr=1e-2)
+    optimizer = torch.optim.Adam(policy.parameters(), lr=1e-2)
 
     running_reward = 0
     MAX_ITER = 300
@@ -14,7 +15,7 @@ def train():
     alpha = .95
 
     for i_episode in range(1):
-        seq = 0 # TODO  generate_random_sequence(N)
+        seq = generate_random_sequence(N)
         state = env.reset(seq)
         best_reward = 0
 
@@ -31,3 +32,21 @@ def train():
 
         # TODO finish episode MC or TD to update the policy
         # finish_episode(optimizer, rewards)
+
+
+
+def generate_random_sequence(N):
+    sequence = ""
+    for i in range (N):
+        epsilon = np.random.uniform(0,1)
+        if epsilon <= 0.25:
+            sequence += "A"
+        elif epsilon <= 0.5:
+            sequence += "U"
+        elif epsilon <= 0.75:
+            sequence+= "G"
+        else:
+            sequence+= "C"
+
+    return sequence
+
