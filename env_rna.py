2c8e6151a4c19cbad8a8ce05b59edad4b7d3912b
NetoPedro <pedroneto_09@hotmail.com>
2019-04-25 12:54:19 +0300

    [EnvRNA] Renamed the file (missing py extension). Also removed the mandatory sequence from initializer. Not it has a default value.

--- env_rna.py
+++ env_rna.py
@@ -1,11 +1,12 @@
-import rna.RNA as RNA
+import rna as rna_module
 
 class EnvRNA:
-    def __init__(self,sequence):
+    def __init__(self,sequence = ""):
         self.reset(sequence)
 
     def reset(self,sequence):
-        self.rna = RNA(sequence)
+        self.rna = rna_module.RNA(sequence)
 
     def step(self,action):
+
         return 0,0,0,0  # This values represent Observation, Reward, Done and info
