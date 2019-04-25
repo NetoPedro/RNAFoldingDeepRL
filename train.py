0deb364e6ade2ecad14c2f9234e7832675d9c90a
NetoPedro <pedroneto_09@hotmail.com>
2019-04-25 12:57:04 +0300

    [Train] Created initial train skeleton

--- train.py
+++ train.py
@@ -1,6 +1,33 @@
+import policy as policy_module
+import env_rna
+import torch.optim.Adam as adam
 # TODO Insert Constants to hold values like max iterations, number of epochs and so on
 
-
+env = env_rna.EnvRNA()
 def train():
-    pass
+    policy = policy_module.Policy()
+    optimizer = adam(policy.parameters(), lr=1e-2)
 
+    running_reward = 0
+    MAX_ITER = 300
+    N = 10
+    alpha = .95
+
+    for i_episode in range(1):
+        seq = 0 # TODO  generate_random_sequence(N)
+        state = env.reset(seq)
+        best_reward = 0
+
+        for t in range(MAX_ITER):
+            rewards = []
+            action = 0 # select_action(policy,state)
+            state, reward, done, _ = env.step(action)
+            rewards.append(reward)
+            reward = max(reward,best_reward)
+            if done:
+                break
+
+        running_reward = running_reward * alpha + best_reward * (1-alpha)
+
+        # TODO finish episode MC or TD to update the policy
+        # finish_episode(optimizer, rewards)
