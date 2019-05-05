f798856651846529b27c417a20dde2a92a32be11
NetoPedro <pedroneto_09@hotmail.com>
2019-05-05 12:08:38 +0300

    [Train] Changed methods to be encapsulated inside a class. This will create a better interface for future approaches

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import train
+import rna
 
 def main():
-    train()
+    r = rna.RNA(train.generate_random_sequence(10))
+    print(r)
+    r.pairing(0,1)
+    print(r)
+    r.pairing(0, 1)
+    r.pairing(1, 2)
+    r.pairing(0, 2)
+    r.pairing(0, 5)
+    r.pairing(3, 4)
+    trainer1 =train.MonteCarloReinforceTrainer()
+    trainer1.train()
 
 
 if __name__ == "__main__":
--- train.py
+++ train.py
@@ -6,40 +6,64 @@ from torch.distributions import Categorical
 
 env = env_rna.EnvRNA()
 
-def train():
-    # TODO Reformulate as A3C algorithm
+class MonteCarloReinforceTrainer:
 
-    running_reward = 0
-    MAX_ITER = 300
-    N = 10
-    alpha = .95
+    def train(self):
+        # TODO Reformulate as A3C algorithm
 
-    policy = policy_module.Policy(N)
-    optimizer = torch.optim.Adam(policy.parameters(), lr=1e-2)
+        running_reward = 0
+        MAX_ITER = 300
+        N = 10
+        alpha = .95
 
-
-    for i_episode in range(1):
-        seq = generate_random_sequence(N)
-        env.reset(seq)
-        state = env.rna.structure_representation
-        ep_reward = 0
-
-        for t in range(MAX_ITER):
-            rewards = []
-            action =  select_action(state,policy)
-            state, reward, done, _ = env.step(action,N)
-            rewards.append(reward)
-            ep_reward += reward
-            if done:
-                break
-
-        running_reward = running_reward * alpha + ep_reward * (1-alpha)
-
-        finish_episode(optimizer, rewards,policy)
+        policy = policy_module.Policy(N)
+        optimizer = torch.optim.Adam(policy.parameters(), lr=1e-2)
 
 
+        for i_episode in range(1):
+            seq = generate_random_sequence(N)
+            env.reset(seq)
+            state = env.rna.structure_representation
+            ep_reward = 0
 
-def select_action(state,policy):
+            for t in range(MAX_ITER):
+                rewards = []
+                action =  select_action_reinforce(state,policy)
+                state, reward, done, _ = env.step(action,N)
+                rewards.append(reward)
+                ep_reward += reward
+                if done:
+                    break
+
+            running_reward = running_reward * alpha + ep_reward * (1-alpha)
+
+            self.finish_episode(optimizer, rewards, policy)
+
+
+
+    def finish_episode(self, optimizer, rewards, policy):
+        R = 0
+        DISCOUNT_FACTOR = 0.99
+        policy_loss = []
+        returns = []
+        for r in rewards[::-1]:
+            R = r+DISCOUNT_FACTOR*R
+            returns.insert(0, R)
+        returns = torch.tensor(returns)
+        returns = (returns - returns.mean()) / (returns.std())
+        for log_prob, R in zip(policy.saved_log_probs, returns):
+            policy_loss.append(-log_prob * R)
+        optimizer.zero_grad()
+        policy_loss = torch.cat(policy_loss).sum()
+        policy_loss.backward()
+        optimizer.step()
+        del policy.rewards[:]
+        del policy.saved_log_probs[:]
+
+
+
+
+def select_action_reinforce(state,policy):
     probs = policy.forward(state)
     m = Categorical(probs)
     action = m.sample()
@@ -62,23 +86,5 @@ def generate_random_sequence(N):
     return sequence
 
 
-def finish_episode(optimizer,rewards,policy):
-    R = 0
-    DISCOUNT_FACTOR = 0.99
-    policy_loss = []
-    returns = []
-    for r in rewards[::-1]:
-        R = r+DISCOUNT_FACTOR*R
-        returns.insert(0, R)
-    returns = torch.tensor(returns)
-    returns = (returns - returns.mean()) / (returns.std())
-    for log_prob, R in zip(policy.saved_log_probs, returns):
-        policy_loss.append(-log_prob * R)
-    optimizer.zero_grad()
-    policy_loss = torch.cat(policy_loss).sum()
-    policy_loss.backward()
-    optimizer.step()
-    del policy.rewards[:]
-    del policy.saved_log_probs[:]
 
-    
+
