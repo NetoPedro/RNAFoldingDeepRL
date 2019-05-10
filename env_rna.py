28d89e53bdffd8e16464b87dfea099241b51095d
NetoPedro <pedroneto_09@hotmail.com>
2019-05-10 11:19:14 +0300

    [EnvRNA] Checking if it is done by analyzing the max number of connection of each pair. (Also counts the crossing connections so it is only an upper bound)

--- env_rna.py
+++ env_rna.py
@@ -4,8 +4,10 @@ class EnvRNA:
     def __init__(self,sequence = ""):
         self.reset(sequence)
 
+
     def reset(self,sequence):
         self.rna = rna_module.RNA(sequence)
+        self.maxValue = min(sequence.count("A"), sequence.count("U")) + min(sequence.count("C"), sequence.count("G"))
 
     def step(self,action_pair,N = 10):
         previous_value = self.rna.free_energy
@@ -13,7 +15,7 @@ class EnvRNA:
         new_value = self.rna.free_energy
         reward = new_value - previous_value
         done = False
-        if N == new_value:
+        if 2*self.maxValue == new_value:
             done = True
         #if reward == 0: reward = -0.1
         return self.rna.structure_representation,reward,done,""  # This values represent Observation, Reward, Done and info
